df8c2f6356bca7645bf292ce63f3fd2f0c238e42
Roman Nikitin <68279389+rnikitin1@users.noreply.github.com>
2022-09-06 15:42:18 +0300

    Update determine environment to multienv, add js-action (#1)

---
+++ js-action/index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const envsJSON = core.getInput('envs');
+    console.log(`Input envs: `, envsJSON);
+
+    const envs = JSON.parse(envsJSON);
+    const githubEvent = github.context.payload;
+    console.log(`The event payload: `, githubEvent);
+
+    const labels = githubEvent.pull_request?.labels || [];
+    console.log('githubEvent labels', labels);
+
+    const labelNames = labels.map(l => l.name);
+    const isStagingSync = labelNames.includes('sync staging');
+    let env = null;
+
+    if (isStagingSync) {
+        const envLabel = labelNames.find(name => !!envs[name]);
+        env = envs[envLabel];
+
+        console.log('Defined env: ', env);
+
+        if (env) {
+            core.setOutput("env", env);
+        }
+    }
+
+    core.setOutput("isDefined", !!env);
+} catch (error) {
+    console.log(`Error message: ${error.message}`);
+    core.setFailed(error.message);
+}
